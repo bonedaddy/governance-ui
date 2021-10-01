7c7e849c5d66e34ebf8873a1d89077bba86fb806
armaniferrante <armaniferrante@gmail.com>
2021-10-01 10:29:49 -0700

    Updates

---
+++ precache-manifest.fb857401557850f9bc2ccd72e93385af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b63d36761ff121e663cc301201037c91",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2df47381b2f81c313598",
+    "url": "./static/css/main.fe57343a.chunk.css"
+  },
+  {
+    "revision": "03642cf14971697a0cb2",
+    "url": "./static/js/2.c1d698f9.chunk.js"
+  },
+  {
+    "revision": "00b7f46e23c35a181934b0a16ae6dc1d",
+    "url": "./static/js/2.c1d698f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2df47381b2f81c313598",
+    "url": "./static/js/main.1f189780.chunk.js"
+  },
+  {
+    "revision": "6e8b7ceada4c8a2d693c",
+    "url": "./static/js/runtime-main.4a3ee0af.js"
+  },
+  {
+    "revision": "4abc2aec9d00d77e4f7bbd30e88f4e2c",
+    "url": "./static/media/dark-horizontal-combined-rainbow.inline.4abc2aec.svg"
+  }
+]);
